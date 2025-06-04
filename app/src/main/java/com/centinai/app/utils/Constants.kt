620036b135bc0d520a19f553104d2ea0a9fa94ea
cabrerayam <cabrerayamileth@gmail.com>
2025-06-03 21:50:50 -0300

    test

--- app/src/main/java/com/centinai/app/utils/Constants.kt
+++ app/src/main/java/com/centinai/app/utils/Constants.kt
@@ -1,6 +1,5 @@
 package com.centinai.app.utils
 
 object Constants {
-    // const val APP_WEB_URL = "https://centinai-8dd1z1c54-juan-martins-projects-34a1b490.vercel.app"
-    const val APP_WEB_URL = "https://centinai-mobile-6203hlu7t-cabrerayams-projects.vercel.app"
+    const val APP_WEB_URL = "https://centinai-8dd1z1c54-juan-martins-projects-34a1b490.vercel.app"
 }
