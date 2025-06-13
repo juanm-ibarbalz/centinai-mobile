b671681e42fbe61c02a21e3ef776f623c58f54ba
cabrerayam <cabrerayamileth@gmail.com>
2025-06-12 23:04:09 -0300

    Test de persistencia de sesion en mobile

--- app/src/main/java/com/centinai/app/utils/Constants.kt
+++ app/src/main/java/com/centinai/app/utils/Constants.kt
@@ -1,5 +1,8 @@
 package com.centinai.app.utils
 
 object Constants {
-    const val APP_WEB_URL = "https://centinai-8dd1z1c54-juan-martins-projects-34a1b490.vercel.app"
+    //const val APP_WEB_URL = "https://centinai-8dd1z1c54-juan-martins-projects-34a1b490.vercel.app"
+    //const val APP_WEB_URL = "https://centinai-pr8f3uhrl-cabrerayams-projects.vercel.app"
+    const val APP_WEB_URL = "https://centinai-frontend-production.up.railway.app/login"
+
 }
